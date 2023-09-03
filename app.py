626667792887100c3040d76b7a7a74937e66b337
Anusha0809 <141609736+Anusha0809@users.noreply.github.com>
2023-09-03 15:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
-from flask import Flask,render_template,request
+from flask import Flask , request ,url_for , render_template
 import numpy as np
-import waitress as server
-app = Flask(__name__)
-@app.route('/liver',methods=['GET','POST'])
-def home():
-    if request.method == 'GET':
-        return render_template('homepage.html')
-    else:
-        age = request.form['age']
-        sex = request.form['sex']
-        total_bilirubin = request.form['chest']
-        direct_bilirubin = request.form['trestbps']
-        alkaline_phosphotase = request.form['chol']
-        alamine_aminotransferase = request.form['fbs']
-        aspartate_aminotransferase = request.form['restecg']
-        total_proteins = request.form['thalach']
-        albumin = request.form['exang']
-        albumin_and_globulin_ratio = request.form['oldpeak']
+import pickle
 
-        model1 = pickle.load(open('./static/liver_model.pkl','rb'))
-        input_data = (age,sex,total_bilirubin,direct_bilirubin,alkaline_phosphotase,alamine_aminotransferase,aspartate_aminotransferase,total_proteins,albumin,albumin_and_globulin_ratio)
-        print(input_data)
-        input_data_as_numpy_array= np.asarray(input_data)
-        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-        prediction = model1.predict(input_data_reshaped)
-        senddata=""
-        if (prediction[0]== 2):
-            senddata='According to the given details person does not have Liver Disease'
-        else:
-            senddata='According to the given details chances of having Liver Disease are High, So Please Consult a Doctor'
-        return render_template('homepage2.html',resultvalue=senddata)
-    if __name__ == '_main_':
-           app.run(debug=True)
+
+sc = pickle.load(open('sc.pkl' , 'rb'))
+
+model = pickle.load(open('model.pkl' , 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict' , methods=['POST'])
+def predict():
+    inputs = [float(x) for x in request.form.values()]
+    inputs = np.array([inputs])
+    inputs = sc.transform(inputs)
+    output = model.predict(inputs)
+    if output < 0.5:
+        output = 0
+    else:
+        output = 1
+    return render_template('result.html' , prediction = output)
+
+if __name__ =='__main__':
+    app.run(debug=True)
